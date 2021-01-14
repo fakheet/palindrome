000574af3f7a156bab22f91ec0805459977adbb5
fakheet <27400@protonmail.ch>
2021-01-14 07:32:49 +0600

    add more comments

--- src/main/java/com/github/fakheet/palindrome/Core.java
+++ src/main/java/com/github/fakheet/palindrome/Core.java
@@ -1,8 +1,12 @@
 package com.github.fakheet.palindrome;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
 
 public class Core {
+    // hashmap containing player objects, uses player name as a key
     private HashMap<String, Player> players;
     public List<Player> leaderboards;
 
@@ -11,13 +15,17 @@ public class Core {
         leaderboards = new ArrayList<>();
     }
 
+    // adds a new player to the game
     public void addPlayer(Player player) {
         this.players.put(player.getName(), player);
     }
 
     public void takeInput(String input, String playerName) throws Exception {
+        // first check if the phrase is a palindrome
         if (isPalindrome(input)) {
             Player player = getPlayer(playerName);
+            // then if the player hasn't used this phrase, add it to their history and give them points equal to length
+            // of the phrase, then update the leaderboards
             if (!player.wasInputUsed(input)) {
                 player.addScore(input.length());
                 player.addInput(input);
@@ -27,9 +35,12 @@ public class Core {
     }
 
     public static boolean isPalindrome(String string) {
+        // convert phrase to lowercase and strip of whitespace
         string = string.toLowerCase();
         string = string.replaceAll("\\s+", "");
 
+        // if the string isn't empty, the algorithm starts to check oppositely located characters,
+        // converging at the middle. If at least one pair doesn't match, the phrase isn't a palindrome.
         if (string.length() != 0) {
             int last = string.length() - 1;
             for (int i = 0; i < string.length() / 2; i++) {
@@ -42,9 +53,10 @@ public class Core {
     }
 
     private void updateLeaderboards() {
+        // create an array out of `players` hashmap values and sort that array by the score field, reversed
         ArrayList<Player> newLeaderboards = new ArrayList<>(players.values());
         newLeaderboards.sort(Comparator.comparing(Player::getScore).reversed());
-
+        // cut the resulting array list to the first five or less elements and reassign to the leaderboards field
         int cut = 5;
         if (newLeaderboards.size() < 5) {
             cut = newLeaderboards.size();
--- src/main/java/com/github/fakheet/palindrome/Main.java
+++ src/main/java/com/github/fakheet/palindrome/Main.java
@@ -3,6 +3,5 @@ package com.github.fakheet.palindrome;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
     }
 }
